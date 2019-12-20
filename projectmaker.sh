e2fd9f8b6223dd7a525f064c2d6b4be47851407e
Josh <jocoso@users.noreply.github.com>
2019-12-20 07:05:54 -0500

    First commit

---
+++ projectmaker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+read -p 'Project Name:' p_name
+
+if test -z "$p_name"
+then
+    echo "ERROR: Project name is mandatory"
+    exit 1
+fi
+
+read -p 'Project Writer:' p_author
+
+if test -z "$p_author"
+then
+    p_author="Undefined"
+fi
+
+mkdir $p_name
+cd $p_name
+
+READMENAME="${p_name}_README.md"
+
+touch "$p_name.ino"
+touch $READMENAME
+
+
+
+echo "## $p_name \n\n ### Written by $p_author" >> $READMENAME
